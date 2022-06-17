06ce3526b20c11ba4be7bf3424fd036364402915
Nagalakshmi <ipsnagalakshmi@gmail.com>
2022-06-17 14:17:27 +0530

    first-commit

---
+++ src/main/java/in/nagait/security/PasswordService.java
@@ -0,0 +1,24 @@
+package in.nagait.security;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class PasswordService {
+	public static String encode(String txt) {
+		Encoder encoder=Base64.getEncoder();
+		return encoder.encodeToString(txt.getBytes());
+	}
+	public static String decode(String encodedTxt) {
+		Decoder decoder=Base64.getDecoder();
+		byte[] decode=decoder.decode(encodedTxt);
+		return new String(decode);
+	}
+	
+	public static void main(String[] args) {
+		String encode=PasswordService.encode("NagaIt");
+		System.out.println(encode);
+		String decode=PasswordService.decode(encode);
+		System.out.println(decode);
+	}
+}
